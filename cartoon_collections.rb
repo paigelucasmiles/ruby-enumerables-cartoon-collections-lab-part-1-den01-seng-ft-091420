1017e1a876c7bf319a043b348cb9ebc751bb6a34
Paige Miles <paige@lucas-miles.com>
2020-09-06 23:22:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,19 @@
-def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  
+def greet_characters(array)
+  array.each do |character|
+    puts "Hello #{character}!"
+  end
 end
 
-def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+
+def list_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
